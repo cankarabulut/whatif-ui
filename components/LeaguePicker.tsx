ec6b531c568fd53b56f4e88d55a1a3c8fc61bdd0
cankarabulut <94984154+cankarabulut@users.noreply.github.com>
2025-11-14 01:57:16 +0300

    added bsa and elc leagues and order

--- components/LeaguePicker.tsx
+++ components/LeaguePicker.tsx
@@ -15,11 +15,11 @@ const FD_LEAGUES = [
   { code: 'SA',  name: 'Serie A' },
   { code: 'BL1', name: 'Bundesliga' },
   { code: 'FL1', name: 'Ligue 1' },
-  { code: 'CL',  name: 'Champions League' },
   { code: 'DED', name: 'Eredivisie' },
   { code: 'PPL', name: 'Primeira Liga' },
   { code: 'BSA', name: 'Campeonato Brasileiro Série A'},
-  { code: 'ELC', name: 'Championship'}  
+  { code: 'ELC', name: 'Championship'},
+  { code: 'CL',  name: 'Champions League' }  
 ];
 
 // Şampiyonlar Ligi ve genel round etiketleri
